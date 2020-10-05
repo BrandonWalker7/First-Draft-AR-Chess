ddc0fdb81cfe1ad8316c9e95502e024b12547695
Fernando <fernando.felix@student.csulb.edu>
2020-10-04 21:20:43 -0700

    Sign in Sign Up working

---
+++ Assets/Scripts/Register.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Register : MonoBehaviour
+{
+    public GameObject email;
+    public GameObject password;
+    public GameObject confPass;
+    
+
+    //private string UserName;
+    private string Email;
+    private string Password;
+    private string ConfPass;
+    private string form;
+    private bool EmailValid = false;
+    Firebase.Auth.FirebaseAuth auth;
+    public Button yourButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(RegisterButton);
+    }
+    
+    public void RegisterButton()
+    {
+        print("clicked register");
+        print(Email);
+        print(Password);
+        
+        print(Password);
+        if (Password != "" && Email != "" && ConfPass != "" && (Password == ConfPass))
+        {
+           auth.CreateUserWithEmailAndPasswordAsync(Email, Password).ContinueWith(task => {
+                if (task.IsCanceled)
+                {
+                    Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+                    return;
+                }
+                if (task.IsFaulted)
+                {
+                    Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                    return;
+                }
+
+                // Firebase user has been created.
+                Firebase.Auth.FirebaseUser newUser = task.Result;
+                Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                    newUser.DisplayName, newUser.UserId);
+            });
+        }
+        else if (Password != ConfPass)
+        {
+            print("Passwords must match");
+        }
+        else
+        {
+            print("Fields Must not remain blank");
+        }
+        
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            /*
+            if (userName.GetComponent<InputField>().isFocused){
+                email.GetComponent<InputField>().Select();
+            }
+            */
+            if (email.GetComponent<InputField>().isFocused)
+            {
+                password.GetComponent<InputField>().Select();
+            }
+            if (password.GetComponent<InputField>().isFocused)
+            {
+                confPass.GetComponent<InputField>().Select();
+            }
+
+        }
+
+        //UserName = userName.GetComponent<InputField>().text;
+        Email = email.GetComponent<InputField>().text;
+        Password = password.GetComponent<InputField>().text;
+        ConfPass = confPass.GetComponent<InputField>().text;
+
+    }
+}
---
+++ Assets/Scripts/SignIn.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Auth;
+using UnityEngine.Experimental.UIElements;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SignIn : MonoBehaviour
+
+{
+    public GameObject email;
+    public GameObject password;
+    private string Email;
+    private string Password;
+    public UnityEngine.UI.Button yourButton;
+    Firebase.Auth.FirebaseAuth auth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        UnityEngine.UI.Button btn = yourButton.GetComponent<UnityEngine.UI.Button>();
+        btn.onClick.AddListener(SignInAuth);
+    }
+
+
+    private void Awake()
+    {
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+                auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+
+                // Set a flag here to indicate whether Firebase is ready to use by your app.
+            }
+            else
+            {
+                UnityEngine.Debug.LogError(System.String.Format(
+                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
+    }
+
+    public void SignInAuth()
+    {
+        if (Password != "" && Email != "")
+        {
+            auth.SignInWithEmailAndPasswordAsync(Email, Password).ContinueWith(task => {
+                if (task.IsCanceled)
+                {
+                    Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+                    return;
+                }
+                if (task.IsFaulted)
+                {
+                    Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                    return;
+                }
+            
+                Firebase.Auth.FirebaseUser newUser = task.Result;
+                Debug.LogFormat("User signed in successfully: {0} ({1})",
+                    newUser.DisplayName, newUser.UserId);
+                
+
+
+            });
+
+        }
+
+        else
+        {
+            print("Fields Must not remain blank");
+        }
+    }
+
+   
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (email.GetComponent<InputField>().isFocused)
+            {
+                password.GetComponent<InputField>().Select();
+            }
+        }
+
+        //UserName = userName.GetComponent<InputField>().text;
+        Email = email.GetComponent<InputField>().text;
+        Password = password.GetComponent<InputField>().text;
+    }
+}
